95f49f5682d69d40fefe8a82eeaee0879ba05829
Yamazaki Daisuke <fhctpr@gmail.com>
2017-06-03 18:24:07 +0900

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+#[macro_use]
+extern crate reqwest;
+extern crate serde_json;
+extern crate time;
+extern crate crypto;
+extern crate rustc_serialize;
+extern crate url;
+
+pub mod bitbank;
+
+#[cfg(test)]
+mod tests {}
